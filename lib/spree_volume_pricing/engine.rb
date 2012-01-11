0c438a7c7f45af1010c117bbb4e4bb3e254bbf47
Alberto Vena <kennyadsl@gmail.com>
2012-01-11 11:48:29 +0100

    Solves delete bug (with :allow_destroy on accepts nested attributes)

--- lib/spree_volume_pricing/engine.rb
+++ lib/spree_volume_pricing/engine.rb
@@ -8,7 +8,7 @@ module SpreeVolumePricing
 
       Variant.class_eval do 
         has_many :volume_prices, :order => :position, :dependent => :destroy
-        accepts_nested_attributes_for :volume_prices
+        accepts_nested_attributes_for :volume_prices, :allow_destroy => true
         
         # calculates the price based on quantity
         def volume_price(quantity)
